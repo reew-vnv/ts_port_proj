61c23b5b3ddb507a26c7cb0e58826c8abf635efa
reew <oloandyolo@gmail.com>
2021-12-13 17:06:08 +0300

    clock component

---
+++ ui/src/components/clock/index.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import './style.scss';
+
+export const Clock = () => {
+  const setClock = () => {
+    const hourHand = document.getElementById('hour')!;
+    const minutesHand = document.getElementById('minutes')!;
+    const secondsHand = document.getElementById('seconds')!;
+
+    const now = new Date();
+    const hour = (`0${now.getHours()}`).slice(-2);
+    const minutes = (`0${now.getMinutes()}`).slice(-2);
+    const seconds = (`0${now.getSeconds()}`).slice(-2);
+
+    hourHand.innerHTML = String(hour);
+    minutesHand.innerHTML = String(minutes);
+    secondsHand.innerHTML = String(seconds);
+  };
+
+  useEffect(() => {
+    setInterval(setClock, 1000);
+    setClock();
+  }, []);
+
+  return (
+    <div className="clock">
+      <div id="hour" />
+      :
+      <div id="minutes" />
+      :
+      <div id="seconds" />
+    </div>
+  );
+};
--- ui/src/containers/navigation/index.tsx
+++ ui/src/containers/navigation/index.tsx
@@ -3,11 +3,15 @@ import { routes } from '../../routes';
 import { ItemNavi } from './item_navi';
 import './style.scss';
 import { AlienSVG } from '../../tools/svg';
+import { Clock } from '../../components';
 
 export const Navigation = () => (
   <nav className="navi">
     <ul className="navi_ul">
-      <div className="navi_ul_items">{routes.map((m, i) => <ItemNavi {...m} key={i} />)}</div>
+      <div className="navi_clock">
+        <Clock />
+        <div className="navi_ul_items">{routes.map((m, i) => <ItemNavi {...m} key={i} />)}</div>
+      </div>
       <div className="navi-icon"><AlienSVG size="28px" /></div>
     </ul>
   </nav>
