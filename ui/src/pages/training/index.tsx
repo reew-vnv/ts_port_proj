5aa9308b73b1840fbd6728981d77d6d3cba2fcd3
reew <oloandyolo@gmail.com>
2021-12-20 00:19:36 +0300

    trying ts

--- ui/src/pages/training/index.tsx
+++ ui/src/pages/training/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { TStore } from '../../store';
-// import { changeTraining } from '../../slices/training';
+import { updateTraining } from '../../slices/training';
+
+import './style.scss';
 
 export const Training = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [train, setTrain] = useState('');
   const { training } = useSelector((state: TStore) => state.trainingReducer);
 
-  // const handleChangeTraining = () => {
-  //   dispatch(changeTraining({ training: 'new training' }));
-  // };
+  const handleChangeTraining = () => {
+    dispatch(updateTraining({ training: train }));
+  };
+
+  const handleChangeInput = (e: React.FormEvent<HTMLInputElement>) => {
+    setTrain(e.currentTarget.value);
+  };
+
+  console.log({ training, train });
 
   return (
-    <div>{training}</div>
+    <div className="training">
+      {training}
+      <input type="input" onChange={handleChangeInput} />
+      <button onClick={handleChangeTraining} type="button">
+        Change Training to New Training
+      </button>
+    </div>
   );
 };
