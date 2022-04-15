756caae7f7c6b64a0c92d00a6d9aa3e0b2ac04ad
reew <oloandyolo@gmail.com>
2022-04-15 12:18:37 +0300

    update depend, testing page

--- ui/src/client/pages/testing/index.tsx
+++ ui/src/client/pages/testing/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useTransition } from 'react';
 import axios from 'axios';
 import { ButtonComponent, InputComponent } from '../../components';
+import './style.scss';
 
 const wrapStyles = {
   display: 'flex',
@@ -8,10 +9,16 @@ const wrapStyles = {
   justifyContent: 'center',
   alignItems: 'center',
 };
-const controlsStyles = {
+const controlsStylesWrapper = {
   display: 'flex',
   alignItems: 'end',
   justifyContent: 'space-between',
+  width: '100%',
+};
+const controlsStyles = {
+  display: 'flex',
+  alignItems: 'end',
+  justifyContent: 'space-around',
   width: '400px',
 };
 const dataStyles = {
@@ -32,8 +39,15 @@ export const Testing = () => {
 
   const getFakeTodos = () => {
     axios.get('https://jsonplaceholder.typicode.com/photos')
-      .then((res: any) => {
-        setItems(res.data);
+      .then((res: {data: []}) => {
+        const data: any[] = [...res.data];
+        data.reduce((acc, item) => {
+          const ndata = { ...item };
+          ndata.id += data.length;
+          acc.push(ndata);
+          return acc;
+        }, data);
+        setItems(data);
       });
   };
 
@@ -47,21 +61,23 @@ export const Testing = () => {
   };
 
   return (
-    <div style={wrapStyles as {}}>
-      <div style={controlsStyles}>
-        {isPending && <span>Loading...</span>}
-        <InputComponent
-          onChange={(e:
+    <div className="testing-wrap-styles">
+      <div className="testing-controls-wrap-styles">
+        <div className="testing-controls">
+          <InputComponent
+            onChange={(e:
              { currentTarget: { value: any; };
              }) => onChangeValue(e.currentTarget.value)}
-          label="Search title"
-          value={value}
-          disabled={!(items.length > 0)}
-        />
-        <ButtonComponent label="get fake data" onClick={getFakeTodos} />
+            label="Search title"
+            value={value}
+            disabled={!(items.length > 0)}
+          />
+          <ButtonComponent label="get fake data" onClick={getFakeTodos} />
+        </div>
+        {isPending ? <span>Loading...</span> : <div />}
       </div>
       {filtered.length > 0 ? (
-        <div style={dataStyles}>
+        <div className="testing-data-styles">
           {filtered.map((item) => (
             <div key={item.id}>
               <span>
